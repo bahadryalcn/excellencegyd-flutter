5e93b64a51135327776b372152ee69939699d2a5
Bahadır Halil Yalçın <bahadrhllyalcn@gmail.com>
2020-11-12 15:32:45 +0300

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -240,9 +240,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "81745df90a4d107683cde92603ef3082",
-"/": "81745df90a4d107683cde92603ef3082",
-"main.dart.js": "aae76b33992044a793a4912712971140",
+"index.html": "981975b601b00203a79280a192214f67",
+"/": "981975b601b00203a79280a192214f67",
+"main.dart.js": "8a92aec0bd3810ee4c58e60d8c357a37",
 "manifest.json": "613eea28c8381541ab210009ed05758c",
 "version.json": "6633be12469a725d57465ca444e538f2"
 };
